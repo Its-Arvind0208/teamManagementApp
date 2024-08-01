00fadff88f84e99cdd99b77b9d4d0e91fedda05a
Arvind <sarojarvind456@gmail.com>
2024-08-02 00:49:01 +0530

    first commit

---
+++ src/TeamMembers.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TeamMemberCard from './TeamMemberCard'
+
+const TeamMembers = ({employees,handleEmployeeCardClick,selectedTeam}) => {
+  return (
+   <>
+ {
+      employees.map((employee)=>(
+       <TeamMemberCard handleEmployeeCardClick={handleEmployeeCardClick}
+                       selectedTeam={selectedTeam}
+                       employee={employee}/>
+      ))
+ }
+   </>
+  )
+}
+
+export default TeamMembers
